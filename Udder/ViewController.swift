500f3226998095b90763f75c538dc10c342b079b
Matt Rickards <matt.rickards@mynd.co>
2020-04-20 07:05:47 -0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Matt Rickards on 4/20/20.
+//  Copyright © 2020 Matt Rickards. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
